30fea0abba1e35d2f675ea91ca7aad0ed2c57313
Justin Feng <justinfeng@Debian12.myguest.virtualbox.org>
2026-01-27 12:33:27 -0800

    Compile and run

--- clang/main.c
+++ clang/main.c
@@ -1,11 +1,12 @@
 #include "util.h"
+#include "util2.h"
 #include <stdio.h>
 
 int main(void)
 {
     int a = 5;
     int b = 20;
-    int result= add(a,b);
+    int result = subtract(b,a);
     printf("%i\n",result);
     return 0;
 }
